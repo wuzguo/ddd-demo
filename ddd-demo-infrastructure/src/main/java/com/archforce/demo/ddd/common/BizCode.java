103ee03647f1fcf6bf91f14a14ba773f03ed8938
wuzguo <wuzguo@gmail.com>
2022-04-15 22:14:33 +0800

    优化代码

--- ddd-demo-infrastructure/src/main/java/com/archforce/demo/ddd/common/BizCode.java
+++
@@ -1,8 +0,0 @@
-package com.archforce.demo.ddd.common;
-
-public class BizCode {
-
-    public static final String BIZ_ONE = "ali.cola.demo.bizOne"; //biz one
-
-    public static final String BIZ_TWO = "ali.cola.demo.bizTwo"; //biz two
-}
--- ddd-demo-infrastructure/src/main/java/com/archforce/demo/ddd/tunnel/database/MetricMapper.java
+++ ddd-demo-infrastructure/src/main/java/com/archforce/demo/ddd/tunnel/database/MetricMapper.java
@@ -8,15 +8,54 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface MetricMapper {
 
-    int create(MetricDO dataObject);
+    /**
+     * 创建指标
+     *
+     * @param metric {@link MetricDO}
+     * @return 创建结果
+     */
+    int create(MetricDO metric);
 
+    /**
+     * 根据用户ID查询
+     *
+     * @param userId 用户ID
+     * @return @return {@link MetricDO}
+     */
     List<MetricDO> listByUserId(@Param("userId") String userId);
+
+    /**
+     * 查询主指标
+     *
+     * @param userId     用户ID
+     * @param mainMetric 主指标ID
+     * @return {@link MetricDO}
+     */
 
     List<MetricDO> listByMainMetric(@Param("userId") String userId, @Param("mainMetric") String mainMetric);
 
+    /**
+     * 查询子指标
+     *
+     * @param userId    用户ID
+     * @param subMetric 子指标ID
+     * @return {@link MetricDO}
+     */
     List<MetricDO> listBySubMetric(@Param("userId") String userId, @Param("subMetric") String subMetric);
 
-    int delete(@Param("id") String id, @Param("modifier") String modifier);
+    /**
+     * 删除
+     *
+     * @param id       指标ID
+     * @param modifier 修改人
+     */
+    void delete(@Param("id") String id, @Param("modifier") String modifier);
 
-    MetricDO getById(@Param("id") String id);
+    /**
+     * 查询
+     *
+     * @param id 指标ID
+     * @return {@link MetricDO}
+     */
+    MetricDO select(@Param("id") String id);
 }
