5f9c7f549767551382029ec69d8eab80c02062d7
wuzguo <wuzguo@gmail.com>
2022-04-18 20:13:02 +0800

    优化代码，践行DDD

--- ddd-demo-controller/src/main/java/com/archforce/demo/ddd/controller/MetricsController.java
+++ ddd-demo-controller/src/main/java/com/archforce/demo/ddd/controller/MetricsController.java
@@ -6,10 +6,14 @@ import com.archforce.demo.ddd.api.IMetricsService;
 import com.archforce.demo.ddd.dto.ATAMetricAddCmd;
 import com.archforce.demo.ddd.dto.ATAMetricQuery;
 import com.archforce.demo.ddd.dto.clientobject.ATAMetricCO;
+import io.swagger.annotations.Api;
 import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Zak
  */
 
+@Slf4j
+@Validated
 @RestController
+@RequestMapping("/api")
+@Api(tags = "客户资料表")
 public class MetricsController {
 
     @Resource
