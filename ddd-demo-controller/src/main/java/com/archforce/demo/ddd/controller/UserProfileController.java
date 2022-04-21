24d250fa724d5b23c62cb88318c679b9938117fb
wuzguo <wuzguo@gmail.com>
2022-04-21 17:39:12 +0800

    优化代码

--- ddd-demo-controller/src/main/java/com/archforce/demo/ddd/controller/UserProfileController.java
+++ ddd-demo-controller/src/main/java/com/archforce/demo/ddd/controller/UserProfileController.java
@@ -18,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,7 @@ public class UserProfileController {
         return userProfileService.addUserProfile(userProfileAddCmd);
     }
 
-    @PostMapping(value = "/user/{userId}")
+    @PutMapping(value = "/user/{userId}")
     public Response updateUser(@PathVariable("userId") @Validated @NotNull String userId,
         @RequestBody UserProfileUpdateCmd profileUpdateCmd) {
         profileUpdateCmd.getUserProfile().setUserId(userId);
